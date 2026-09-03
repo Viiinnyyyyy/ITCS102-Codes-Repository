bcbfc3a989b96b87c6b124ce1a0ddd266f28ed93
Viiinnyyyyy <vincekllymarlinga@gmail.com>
2026-09-03 12:36:25 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,11 @@
+a = 38
+b = 8
+sum = a + b
+diff = a - b
+print("the sum of ",a,"plus ",b,"is ", sum)
+print("the sum of ",a,"minus ",b,"is ", diff)
+print("the sum of ",a,"times ",b,"is ", a*b)
+print("the sum of ",a,"devided by ",b,"is ", a/b)
+print("the sum of ",a,"raised to the power of ",b,"is ", a**b)
+print(a,"//",b," is ",a//b)
+print("the remainder of",a,"devided by",b," is ",a%b) 

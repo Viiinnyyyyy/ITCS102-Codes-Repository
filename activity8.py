4c5f26439755ea744f0cd963e80ab52cba923e47
Viiinnyyyyy <vincekllymarlinga@gmail.com>
2026-09-03 12:32:39 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,13 @@
+hobbies = ""
+
+h = input("What are your hobbies? --> ")
+hobbies += h + ", "
+
+h = input("Why did you like it? --> ")
+hobbies += h + ", "
+
+h = input("Is it fun? --> ")
+hobbies += h + ", "
+
+
+print ("so my hobbie is ",hobbies)

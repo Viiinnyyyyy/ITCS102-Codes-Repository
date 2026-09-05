a0ad9201754322a3288b74c737b36825d5a3ff8c
Viiinnyyyyy <vincekllymarlinga@gmail.com>
2026-09-05 09:45:36 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,23 @@
+#integer
+girls = 17
+boys = 28
+age = 18
+
+#float
+myHeight = 1.74
+temperature = 36.5
+weight = 45.8
+
+#string 
+myName = "vinny"
+school = "DLL"
+fav_color = "green" 
+
+#boolean
+is_in_love = True
+is_a_boy = True
+is_always_sleepy = True
+ 
+print("my name is ", myName)
+print("i am ",age)
+print("my weight is ", weight)

9f0d61a197434db5f7211b05c0bffd790fd5f7ef
Viiinnyyyyy <vincekllymarlinga@gmail.com>
2026-09-08 12:44:05 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,11 @@
+user = 'vince'
+passw = 'hi'
+
+
+u = input("Input username ---> ")
+p = input("Input password ---> ")
+
+if user == u and p == passw :
+	print("ACCSESS GRANTED")
+else :
+	print("ACCESS DENIED")
